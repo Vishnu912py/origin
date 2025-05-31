c72999cec17c86a0a859da7178c3f516a4422a93
Vishnu <vnnrocks@gmail.com>
2025-05-31 19:57:02 +0530

    prog12.java

--- src/prog12.java
+++ src/prog12.java
@@ -1,7 +1,12 @@
-package newPrograms;
+import java.util.StringTokenizer;
 
 public class prog12 {
     public static void main(String[] args){
         System.out.println("hello ");
+        String s = "This is a StringTokenizer Demonstration";
+        StringTokenizer st = new StringTokenizer(s," ");
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
     }
 }
